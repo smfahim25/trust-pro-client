eeff23ed52f448977baabb2622c4cb848bbe762e
fahimhossen <fahim.hossen@koneksys.com>
2024-09-01 12:50:05 +0600

    update trade limit

--- src/api/getApiURL.js
+++ src/api/getApiURL.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = "https://cryptotradeapi.solvyxx.com/api/v1";
+// const API_BASE_URL = "https://cryptotradeapi.solvyxx.com/api/v1";
 // const API_BASE_URL ="https://metaadmin.online/api/v1";
 // const API_BASE_URL = "http://localhost:5000/api/v1";
 
-// const API_BASE_URL = "https://proit99.online/api/v1";
+const API_BASE_URL = "https://proit99.online/api/v1";
 
 export default API_BASE_URL;
